af7292fd647f6d0325175e527164262fa44e6f8a
ehtisham mohd <125719040+Ehtisham9198@users.noreply.github.com>
2024-01-22 11:56:00 +0530

    pass

--- DataBase/connection.js
+++ DataBase/connection.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 
 export async function connectdb() {
-  await mongoose.connect('mongodb+srv://b522035:<password>@cluster0.vlgcgga.mongodb.net/?retryWrites=true&w=majority');
+  await mongoose.connect('mongodb+srv://b522035:Ehtisham@cluster0.vlgcgga.mongodb.net/?retryWrites=true&w=majority');
 }
